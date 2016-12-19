843a22368ce12573666e252e9c2ef8f845afebbc
Yoga Prakoso <yogaprakoso90@gmail.com>
2016-12-19 21:34:02 +0000

    Adds conditionals exercises

---
+++ go-outside.js
@@ -0,0 +1,11 @@
+var temeprature = 12;
+
+if (temeprature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temeprature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+    console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
